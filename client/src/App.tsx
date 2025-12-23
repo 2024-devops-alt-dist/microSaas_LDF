988453287c88327c8c5b8b88a42ecbf5ca9b1042
lucio.dellafelice <lucio.dellafelice@origins-digital.com>
2025-12-23 16:27:47 +0100

    feat(circles): request to join a circle

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import MyCirclesView from './features/circles/components/MyCirclesView';
 import ApiHealth from './pages/ApiHealth';
+import AvailableCirclesView from './features/circles/components/AvailableCirclesView';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MyCirclesView />} />
+        <Route path="/explore" element={<AvailableCirclesView />} />
         <Route path="/health" element={<ApiHealth />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
