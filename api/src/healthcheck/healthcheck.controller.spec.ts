20ca75be880c2d5cd37ef5f732ba11c735963361
lucio.dellafelice <lucio.dellafelice@origins-digital.com>
2026-01-07 00:48:15 +0100

    fix: healthcheck test dependencies

--- api/src/healthcheck/healthcheck.controller.spec.ts
+++ api/src/healthcheck/healthcheck.controller.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthcheckController } from './healthcheck.controller';
 
+import { HealthCheckService, MongooseHealthIndicator } from '@nestjs/terminus';
+
 describe('HealthcheckController', () => {
   let controller: HealthcheckController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HealthcheckController],
+      providers: [
+        {
+          provide: HealthCheckService,
+          useValue: {
+            check: jest.fn(),
+          },
+        },
+        {
+          provide: MongooseHealthIndicator,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<HealthcheckController>(HealthcheckController);
