3a72d601a864d13d0efade3ece1655a48c4eed1a
lucio.dellafelice <lucio.dellafelice@origins-digital.com>
2026-02-04 15:20:24 +0100

    fix(typescript): allow unused variables

--- api/src/notifications/notifications.service.ts
+++ api/src/notifications/notifications.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { CreateNotificationDto } from './dto/create-notification.dto';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
