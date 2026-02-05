6baf11ca4b6bdaf3ae89b29b86460686e5fc12b0
lucio.dellafelice <lucio.dellafelice@origins-digital.com>
2026-02-05 15:04:45 +0100

    fix(api): fix join circle request

--- api/src/requests/requests.module.ts
+++ api/src/requests/requests.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RequestsService } from './requests.service';
 import { RequestsController } from './requests.controller';
-import { RequestEntity, RequestSchema } from './schemas/request.schema';
+import { CircleRequest, RequestSchema } from './schemas/request.schema';
 import { CirclesModule } from '../circles/circles.module';
 import { UsersModule } from '../users/users.module';
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: RequestEntity.name, schema: RequestSchema },
+      { name: CircleRequest.name, schema: RequestSchema },
     ]),
     CirclesModule,
     UsersModule,
